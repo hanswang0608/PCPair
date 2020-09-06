98fa6ae7def5e7f6db27aba1a13edb41a964b37d
hanswang0608 <57380995+hanswang0608@users.noreply.github.com>
2020-09-06 18:34:37 -0400

    Testing with 2 remotes

--- server.js
+++ server.js
@@ -43,8 +43,6 @@ mongoose.connect(mongoURI,
 const port = process.env.PORT || 80;
 app.listen(port, () => console.log(`Server started on port ${port}`));
 
-console.log('Testing git push to two remotes');
-
 // scrape();
 
 // --- DISABLED --- 
